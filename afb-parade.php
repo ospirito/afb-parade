04bab673a042d095545d1f36b1aeef977e92cede
Oliver Spirito <ospirito@onetrust.com>
2023-08-29 22:28:50 -0400

    Increment version

--- afb-parade.php
+++ afb-parade.php
@@ -5,7 +5,7 @@
  * Description:       Custom blocks for Atlanta Freedom Bands
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.0.3
+ * Version:           0.0.4
  * Author:            Oliver Spirito
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
